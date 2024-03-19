a1c64f62cb291f1207d334a04bac31fb34a81b01
prithvirajc <prithvirajc@infinite.com>
2024-03-19 17:06:26 +0530

    rowDetailsPanel

--- src/components/custom/table/body/RowDetailsPanel.jsx
+++ src/components/custom/table/body/RowDetailsPanel.jsx
@@ -1,70 +1,83 @@
-import { Button } from "@mui/material";
-import "./RowDetailsPanel.scss";
-
 import React from "react";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+import "./RowDetailsPanel.scss";
+import { chunkArray } from "../../../../utils/globalMethods";
 
 export default function RowDetailsPanel(props) {
+  const { data, expandedRowDetailPanelJson } = props;
 
-    const { data, expandedRowDetailPanelJson } = props
-
-  const chunkArray = (arr, size) => {
-    const chunks = [];
-    for (let i = 0; i < arr.length; i += size) {
-      chunks.push(arr.slice(i, i + size));
-    }
-    return chunks;
+  const renderRowDetailsPanelHeader = () => {
+    return expandedRowDetailPanelJson?.expandedRowDetailPanelHeader?.timeDetails?.map(
+      (detail) => (
+        <div key={detail.uniqueKey}>
+          <div className="data-label">{detail.label}:</div>
+          <div className="data-value">{data[detail.uniqueKey]}</div>
+        </div>
+      )
+    );
   };
 
-  const maxColumns = 3;
-  const numRows = Math.ceil(
-    (expandedRowDetailPanelJson?.expandedRowDetailPanelBody
-      ?.userTransportDetails?.length || 0) / maxColumns
-  );
+  const renderRowDetailsPanelBody = () => {
+    const maxColumns = 3;
+    const numRows = Math.ceil(
+      (expandedRowDetailPanelJson?.expandedRowDetailPanelBody
+        ?.rowDetailPanelBodyTable?.length || 0) / maxColumns
+    );
 
-  const dynamicDataChunks = chunkArray(
-    expandedRowDetailPanelJson?.expandedRowDetailPanelBody
-      ?.userTransportDetails,
-    numRows
-  );
+    const dynamicDataChunks = chunkArray(
+      expandedRowDetailPanelJson?.expandedRowDetailPanelBody
+        ?.rowDetailPanelBodyTable,
+      numRows
+    );
 
-  return (
-    <div className="expanded-row-details-panel">
-      <div className="top-right">
-        {expandedRowDetailPanelJson?.expandedRowDetailPanelHeader?.timeDetails?.map(
-          (detail) => (
-            <div key={detail.uniqueKey}>
-              <div className="data-label">{detail.label}:</div>
-              <div className="data-value">{data[detail.uniqueKey]}</div>
-            </div>
-          )
-        )}
-      </div>
-      <div className="row-details-panel-body-footer-container">
+    return (
       <div className="body">
-        {dynamicDataChunks?.length && dynamicDataChunks.map((chunk, index) => (
-        <>
-          <div key={index} className="body-column">
-            {chunk.map((detail) => (
-              <div key={detail.uniqueKey} className="data-row">
-                <div className="data-label">{detail.label}:</div>
-                <div className="data-value">{data[detail.uniqueKey]}</div>
+        {dynamicDataChunks?.length &&
+          dynamicDataChunks.map((chunk, index) => (
+            <>
+              <div key={index} className="body-column">
+                {chunk.map((detail) => (
+                  <div key={detail.uniqueKey} className="data-row">
+                    <div className="data-label">{detail.label}</div>
+                    <div className="data-value">{data[detail.uniqueKey]}</div>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-          {(index !== (dynamicDataChunks?.length - 1)) && <div className="vertical-line"></div>}
-        </>
-        ))}
+              {index !== dynamicDataChunks?.length - 1 && (
+                <div className="vertical-line"></div>
+              )}
+            </>
+          ))}
       </div>
+    );
+  };
+
+  const renderRowDetailsPanelFooter = () => {
+    const footerTableColumns = expandedRowDetailPanelJson?.expandedRowDetailPanelFooter?.numColumnBlocks || 2;
+    const footerTableDataChunks = chunkArray(
+      expandedRowDetailPanelJson?.expandedRowDetailPanelFooter
+        ?.rowDetailPanelFooterTable || [],
+      footerTableColumns
+    );
+
+    return (
       <div className="footer">
         <div className="footer-left">
-          {expandedRowDetailPanelJson?.expandedRowDetailPanelFooter?.userClosingDetails?.map(
-            (detail) => (
-              <div key={detail.uniqueKey} className="data-row">
-                <div className="data-label">{detail.label}:</div>
-                <div className="data-value">{data[detail.uniqueKey]}</div>
-              </div>
-            )
-          )}
+          {footerTableDataChunks.map((chunk, index) => (
+            <div key={index} className="footer-block">
+              {chunk.map((row) => (
+                <div key={row.uniqueKey} className="footer-row">
+                  <Typography variant="body2" className="footer-label">
+                    {row.label}
+                  </Typography>
+                  <Typography variant="body2" className="footer-value">
+                    {data[row.uniqueKey]}
+                  </Typography>
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
         <div className="footer-right">
           {expandedRowDetailPanelJson?.expandedRowDetailPanelFooter?.buttons?.map(
@@ -76,6 +89,15 @@ export default function RowDetailsPanel(props) {
           )}
         </div>
       </div>
+    );
+  };
+
+  return (
+    <div className="expanded-row-details-panel">
+      <div className="top-right">{renderRowDetailsPanelHeader()}</div>
+      <div className="row-details-panel-body-footer-container">
+        {renderRowDetailsPanelBody()}
+        {renderRowDetailsPanelFooter()}
       </div>
     </div>
   );
--- src/utils/globalMethods.js
+++ src/utils/globalMethods.js
@@ -1,3 +1,11 @@
 export const toTitleCase = (str) => {
   return str.split("-").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 };
+
+export const chunkArray = (arr, size) => {
+  const chunks = [];
+  for (let i = 0; i < arr.length; i += size) {
+    chunks.push(arr.slice(i, i + size));
+  }
+  return chunks;
+};
